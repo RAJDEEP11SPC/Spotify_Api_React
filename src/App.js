81e4e3af5e3ee62f6ebbe02b279ce0f9ac29e5ca
Rajdeep Sen Gupta <xyzrajdeep@gmail.com>
2022-01-18 20:38:32 +0530

    SPOTIFY API USING REACT

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React , {useState , useEffect} from 'react'
+import './App.css'
+import axios from 'axios'
+
+const App = () => {
+const CLIENT_ID = "fdae41c9aa6c481280a86d6a8d480974"
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
+const [token , setToken] = useState('')
+const [ searchkey , setSearchKey ] = useState('')
+const [ artists , setArtists] = useState('')
+
+
+useEffect( () => {
+  const hash = window.location.hash
+  let token = window.localStorage.getItem("token")
+
+  if(!token && hash){
+    token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+    
+    window.location.hash = ''
+    window.localStorage.setItem("token", token)
+   
+  }
+
+  setToken(token)
+
+}, [])
+
+const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+}
+
+const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+      Authorization: `Bearer ${token}`
+  },
+    params: {
+      q: searchkey,
+      type: "artist"
+    }
+})
+
+  console.log(data)
+  setArtists(data.artists.items)
+}
+ 
+const renderArtists = () => {
+  return artists.map(artist => (
+      <div key={artist.id}>
+          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+          {artist.name}
+      </div>
+  ))
+}
+
+
+
+  return (
+    <div className='App'>
+      <h1>Spotify App</h1>
+      {!token ?
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>LOGIN TO SPOTIFY</a>
+      : <button onClick={ logout }>Logout</button>
+      } 
+
+      {token ?
+      
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+            
+            <button type={"submit"}>Search</button>
+        
+        </form>
+        
+        : <h2>Please login</h2>
+
+      }
+
+
+
+      {artists && renderArtists()}
+      
+    </div>
+  )
+}
+
+export default App
